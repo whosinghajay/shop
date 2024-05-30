ae97711a57225290c54ef397ab841afe2daf4f16
Ajay Singh <whosinghajay@gmail.com>
2024-05-30 23:18:00 +0530

    reviews section created(not fully)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all ">
@@ -27,7 +29,7 @@ const Navbar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href={"/api/auth/logout"}
+                    href={"/api/auth/dashboard"}
                     className={buttonVariants({ size: "sm", variant: "ghost" })}
                   >
                     Dashboard ✨
@@ -60,17 +62,17 @@ const Navbar = () => {
                   Login
                 </Link>
 
-                <div className="h-8 w-px bg-zinc-200 hidden sm:block"/>
-                  <Link
-                    href={"/configure/upload"}
-                    className={buttonVariants({
-                      size: "sm",
-                      className: "hidden sm:flex items-center gap-1",
-                    })}
-                  >
-                    Create Case
-                    <ArrowRight className="ml-1.5 h-5 w-5" />
-                  </Link>
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+                <Link
+                  href={"/configure/upload"}
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create Case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
               </>
             )}
           </div>
